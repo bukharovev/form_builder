b4f9d93f4387c022dab07552509aedae5de6e772
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-06-29 01:30:59 +0300

    small refactoring

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,67 +1,44 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
+require_relative 'tag'
 
 # module
 module HexletCode
   class Error < StandardError; end
 
-  def self.form_for(entity, url: '#', &block)
-    return unless block
+  class << self
+    def form_for(entity, url: '#', &block)
+      return unless block
 
-    @entity = entity.to_h
-    @form = %(<form action="#{url}" method="post">)
-    yield self if block
-    %(#{@form}\n</form>)
-  end
-
-  def self.add_label(value)
-    label = Tag.build('label', for: value) { value.capitalize }
-    @form = %(#{@form}\n  #{label})
-  end
-
-  def self.input(field, *attributes)
-    return unless @entity.key? field
-
-    attributes_hash = attributes.first || {}
-    add_label field
-    value = @entity[field]
-    tag = if attributes_hash.fetch(:as, nil)
-            Tag.build('textarea', cols: '20', rows: '40', name: field, **attributes_hash) { value }
-          else
-            Tag.build('input', type: 'text', name: field, value: value, **attributes_hash)
-          end
-
-    @form = %(#{@form}\n  #{tag})
-  end
-
-  def self.submit(button_name = 'Save')
-    tag = Tag.build('input', type: 'submit', value: button_name, name: 'commit')
-    @form = %(#{@form}\n  #{tag})
-  end
-
-  # tag module
-  module Tag
-    def self.build_attributes(attributes)
-      attributes.reduce('') do |res, (key, value)|
-        res = %(#{res} #{key}="#{value}") if value
-        res
-      end
+      @entity = entity
+      @form = %(<form action="#{url}" method="post">)
+      yield self if block
+      %(#{@form}\n</form>)
     end
 
-    def self.build(tag_name, *attributes_row, &block)
-      tag_body = yield if block
-      attributes_hash = attributes_row.first
+    def add_label(value)
+      label = Tag.build('label', for: value) { value.capitalize }
+      @form = %(#{@form}\n  #{label})
+    end
 
-      result = if attributes_hash
-                 attributes = build_attributes attributes_hash
-                 %(<#{tag_name}#{attributes}>)
-               else
-                 %(<#{tag_name}>)
-               end
+    def input(field, attributes = {})
+      # return if @entity[field].nil?
 
-      result = %(#{result}#{tag_body}</#{tag_name}>) if tag_body
-      result
+      add_label field
+      value = @entity[field]
+      tag = if attributes.fetch(:as, nil)
+              Tag.build('textarea', cols: '20', rows: '40', name: field, **attributes) { value }
+            else
+              Tag.build('input', type: 'text', name: field, value: value, **attributes)
+            end
+
+      @form = %(#{@form}\n  #{tag})
+    end
+
+    def submit(button_name = 'Save')
+      tag = Tag.build('input', type: 'submit', value: button_name, name: 'commit')
+      @form = %(#{@form}\n  #{tag})
     end
   end
 end
