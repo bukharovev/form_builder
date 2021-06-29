44834ebe8157b953484f79420181f0cd2330d46e
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-06-29 03:09:23 +0300

    refactoring

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,40 +5,61 @@ require_relative 'tag'
 
 # module
 module HexletCode
-  class Error < StandardError; end
-
   class << self
     def form_for(entity, url: '#', &block)
       return unless block
 
+      @tags = []
       @entity = entity
-      @form = %(<form action="#{url}" method="post">)
-      yield self if block
-      %(#{@form}\n</form>)
-    end
 
-    def add_label(value)
-      label = Tag.build('label', for: value) { value.capitalize }
-      @form = %(#{@form}\n  #{label})
+      yield self if block
+
+      build_form(@tags, url)
     end
 
     def input(field, attributes = {})
       # return if @entity[field].nil?
 
-      add_label field
       value = @entity[field]
-      tag = if attributes.fetch(:as, nil)
-              Tag.build('textarea', cols: '20', rows: '40', name: field, **attributes) { value }
+      as = attributes.fetch(:as, nil)
+      tag = case as
+            when :text
+              build_textarea(field, attributes, value)
             else
-              Tag.build('input', type: 'text', name: field, value: value, **attributes)
+              build_input(field, attributes, value)
             end
 
-      @form = %(#{@form}\n  #{tag})
+      @tags << tag
     end
 
     def submit(button_name = 'Save')
       tag = Tag.build('input', type: 'submit', value: button_name, name: 'commit')
-      @form = %(#{@form}\n  #{tag})
+      @tags << tag
+    end
+
+    private
+
+    def add_label(value)
+      label = Tag.build('label', for: value) { value.capitalize }
+      @tags << label
+    end
+
+    def build_form(tags, form_url)
+      form_open_tag = %(<form action="#{form_url}" method="post">)
+      form_closed_tag = '</form>'
+      form_body = tags.reduce('') { |acc, tag| %(#{acc}\n  #{tag}) }
+
+      %(#{form_open_tag}#{form_body}\n#{form_closed_tag})
+    end
+
+    def build_textarea(field, attributes, value)
+      add_label field
+      Tag.build('textarea', cols: '20', rows: '40', name: field, **attributes) { value }
+    end
+
+    def build_input(field, attributes, value)
+      add_label field
+      Tag.build('input', type: 'text', name: field, value: value, **attributes)
     end
   end
 end
