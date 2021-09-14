33472fd675ca6b92be05449e65414918600a3825
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-14 21:45:20 +0300

    fix linter error

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -6,20 +6,24 @@ module HexletCode
 
   class FormBuilder
     class << self
+      DEFAULT_FORM_ATTRIBUTES = {
+        action: '#',
+        method: 'post'
+      }.freeze
+
       def build(form)
-        inputs = form.inputs
+        inputs, form_attributes = *form
         form.options[:action] = form.options.delete :url
-        options = { action: '#', method: 'post' }.merge(form.options)
+        updated_form_attributes = DEFAULT_FORM_ATTRIBUTES.merge(form_attributes)
 
         builded_tags = inputs.map do |input|
-          type = input[:type]
-          attributes = input[:attributes]
+          type, attributes = input.values_at(:type, :attributes)
           tag = Tags.get_by_type(type)
           tag.build(**attributes)
         end
 
         form_body_str = "#{builded_tags.reduce('') { |acc, tag| "#{acc}\n  #{tag}" }}\n"
-        Tags::Tag.build('form', **options) { form_body_str }
+        Tags::Tag.build('form', **updated_form_attributes) { form_body_str }
       end
     end
   end
