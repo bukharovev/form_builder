90fb50abe26e5455239efbfc3ddb55483ac9e946
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-15 21:53:23 +0300

    fix naming

--- lib/hexlet_code/tags/input.rb
+++ lib/hexlet_code/tags/input.rb
@@ -5,10 +5,10 @@ module HexletCode
     autoload :Tag, 'hexlet_code/tags/tag'
 
     class Input
-      def self.build(options)
+      def self.build(attributes)
         result = []
-        result << Tag.build('label', for: options[:name]) { options[:name].capitalize }
-        result << Tag.build('input', type: 'text', **options)
+        result << Tag.build('label', for: attributes[:name]) { attributes[:name].capitalize }
+        result << Tag.build('input', type: 'text', **attributes)
         result.join("\n  ")
       end
     end
