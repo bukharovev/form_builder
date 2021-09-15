79ab615ad0f2f4ec93644de43d73b144de03cdbf
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-15 22:34:21 +0300

    move default attributes to constant

--- lib/hexlet_code/tags/input.rb
+++ lib/hexlet_code/tags/input.rb
@@ -3,10 +3,15 @@
 module HexletCode
   module Tags
     class Input
+      DEFAULT_ATTRIBUTES = {
+        type: 'text'
+      }.freeze
+
       def self.build(attributes)
+        updated_attributes = DEFAULT_ATTRIBUTES.merge(attributes)
         result = []
         result << Label.build(attributes)
-        result << Tag.build('input', type: 'text', **attributes)
+        result << Tag.build('input', **updated_attributes)
         result.join("\n  ")
       end
     end
