e7d6da7a5a27d64308b0b174a86d0a9c2f67b3ba
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-15 14:22:50 +0300

    refactoring

--- lib/hexlet_code/tags/textarea.rb
+++ lib/hexlet_code/tags/textarea.rb
@@ -5,12 +5,16 @@ module HexletCode
     autoload :Tag, 'hexlet_code/tags/tag'
 
     class Textarea
+      DEFAULT_ATTRIBUTES = {
+        cols: '20',
+        rows: '40'
+      }.freeze
       class << self
         def build(value: '', **options)
-          updated_options = { as: 'text' }.merge(options)
+          updated_options = DEFAULT_ATTRIBUTES.merge(options)
           result = []
           result << Tag.build('label', for: options[:name]) { options[:name].capitalize }
-          result << Tag.build('textarea', cols: '20', rows: '40', **updated_options) { value }
+          result << Tag.build('textarea', **updated_options) { value }
           result.join("\n  ")
         end
       end
