cf777374cd4924c568a679b43c27e52ef65ef7c7
Евгений Бухаров <evgeny@MacBook-Pro-Evgenij.local>
2021-09-23 01:09:47 +0300

    add selected_value

--- lib/hexlet_code/inputs/select.rb
+++ lib/hexlet_code/inputs/select.rb
@@ -7,10 +7,12 @@ module HexletCode
         def build(options: {}, **attributes)
           depth = options.fetch(:depth, 0)
           updated_options = options.merge({ depth: depth + 1 })
-          select_body = build_select_options(attributes[:collection], options: updated_options)
+          selected_value = attributes[:value]
+          select_body = build_select_body(attributes[:collection], options: updated_options,
+                                                                   selected_value: selected_value)
 
           select_body_str = select_body.join("\n")
-          input('select', attributes: attributes.except(:collection),
+          input('select', attributes: attributes.except(:collection, :value),
                           options: options.merge({ with_nested_body: true })) do
             select_body_str
           end
@@ -18,9 +20,10 @@ module HexletCode
 
         private
 
-        def build_select_options(collection, options: {})
-          collection.each_with_index.map do |value, index|
-            if index.zero?
+        def build_select_body(collection, options: {}, selected_value: nil)
+          selected_value ||= collection.first
+          collection.each_with_index.map do |value, _index|
+            if selected_value == value
               Tag.build('option', attributes: { value: value, selected: nil }, options: options) { value }
             else
               Tag.build('option', attributes: { value: value }, options: options) { value }
